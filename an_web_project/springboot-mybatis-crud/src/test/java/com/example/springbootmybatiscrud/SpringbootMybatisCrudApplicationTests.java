ea2cc1db81bac26da072f954ec35e4ba89c0e181
lixinan1234 <2489460735@qq.com>
2024-06-07 23:53:56 +0800

    mybatis(基础操作——删除)

--- an_web_project/springboot-mybatis-crud/src/test/java/com/example/springbootmybatiscrud/SpringbootMybatisCrudApplicationTests.java
+++ an_web_project/springboot-mybatis-crud/src/test/java/com/example/springbootmybatiscrud/SpringbootMybatisCrudApplicationTests.java
@@ -18,7 +18,7 @@ class SpringbootMybatisCrudApplicationTests {
         if(delete == 1){
             System.out.println("删除成功！");
         }else{
-            System.out.println("删除失败！");
+            System.out.println("");
         }
     }
 
