47eaf635d8f6b405ddf62e4b9ca3d10816d3bc06
lixinan1234 <2489460735@qq.com>
2024-07-26 22:51:47 +0800

    新增菜品业务代码开发

---
+++ an_web_project/springbootWeb-anli/src/main/java/com/anli/config/OssConfiguration.java
@@ -0,0 +1,12 @@
+package com.anli.config;
+
+/**
+ * @author:lixinan
+ * @email:2489460735@qq.com
+ * @desc:
+ * @datetime: 2024/7/26 15:45
+ */
+
+//配置类，用于创建aliOss
+public class OssConfiguration {
+}
