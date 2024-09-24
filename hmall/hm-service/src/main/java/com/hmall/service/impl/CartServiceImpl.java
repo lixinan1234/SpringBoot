a8c78154b9f3cf68f02e0c83920fe33ec116e7da
李新安 <2489460735@qq.com>
2024-09-24 08:25:20 +0800

    微服务拆分

--- hmall/hm-service/src/main/java/com/hmall/service/impl/CartServiceImpl.java
+++ hmall/hm-service/src/main/java/com/hmall/service/impl/CartServiceImpl.java
@@ -113,14 +113,14 @@ public class CartServiceImpl extends ServiceImpl<CartMapper, Cart> implements IC
     }
 
     private void checkCartsFull(Long userId) {
-        Long count = lambdaQuery().eq(Cart::getUserId, userId).count();
+        int count = lambdaQuery().eq(Cart::getUserId, userId).count();
         if (count >= 10) {
             throw new BizIllegalException(StrUtil.format("用户购物车课程不能超过{}", 10));
         }
     }
 
     private boolean checkItemExists(Long itemId, Long userId) {
-        Long count = lambdaQuery()
+        int count = lambdaQuery()
                 .eq(Cart::getUserId, userId)
                 .eq(Cart::getItemId, itemId)
                 .count();
---
+++ hmall/item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -0,0 +1,18 @@
+package com.hmall.item;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@MapperScan("com.hmall.item.mapper")
+@SpringBootApplication
+public class ItemApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApplication.class, args);
+    }
+
+
+}
