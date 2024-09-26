c9f53b9c19993348fd548dbdedf3b2d64bfd1cab
李新安 <2489460735@qq.com>
2024-09-26 22:41:24 +0800

    微服务-》交易服务模块拆分

--- hmall/item-service/src/main/java/com/hmall/item/ItemApplication.java
+++ hmall/item-service/src/main/java/com/hmall/item/ItemApplication.java
@@ -1,13 +1,15 @@
 package com.hmall.item;
 
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
-
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
 @MapperScan("com.hmall.item.mapper")
 @SpringBootApplication
 public class ItemApplication {
--- hmall/item-service/src/test/java/com/hmall/item/ItemApplication.java
+++ hmall/item-service/src/test/java/com/hmall/item/ItemApplication.java
@@ -1,16 +1,21 @@
 package com.hmall.item;
 
+import com.hmall.api.config.DefaultFeignConfig;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 
+@EnableFeignClients(basePackages = "com.hmall.api.client", defaultConfiguration = DefaultFeignConfig.class)
 @MapperScan("com.hmall.item.mapper")
 @SpringBootApplication
 public class ItemApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemApplication.class, args);
     }
+
+
 }
